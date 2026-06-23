a2148a3b37f1e701cb16fc2aef17756441cf1257
Aerisorionvale <246976681@qq.com>
2026-06-23 13:23:26 +0800

    Update db_api.py

--- db_api.py
+++ db_api.py
@@ -11,7 +11,7 @@ def get_db_conn():
         database=st.secrets["TIDB_DATABASE"],
         charset="utf8mb4",
         cursorclass=pymysql.cursors.DictCursor,
-        ssl=True}  
+        ssl=True  
     )
     return conn
 
